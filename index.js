5a98b598a830da000f07cead5b7330c34ae2b2f3
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-04 18:52:47 -0500

    chore: create base addon

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  name: require('./package').name,
+
+  // enable file-watching / live-reload
+  // isDevelopingAddon: () => true,
+
+  options: {
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
+  },
+};
